bca42742ea158aee9a8c0bce6d91f5b2bc077071
GianTOpy <gianfry2000@tiscali.it>
2022-01-30 00:03:40 +0100

    versione2

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,16 @@
-document.querySelector('h1').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
-
 let myImage = document.querySelector('#peepos'); // con # seleziona solo ciò ceh identifichi con un id nell'html
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/peepolove.png') {
       myImage.setAttribute('src','images/peepo2.png');
-    } else {
+    }
+    else {
       myImage.setAttribute('src','images/peepolove.png');
     }
 }
 
-let myButton = document.querySelector('button');
+let myButton = document.querySelector('#changename');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
@@ -34,4 +31,11 @@ if(!localStorage.getItem('name')) {
 }
 myButton.onclick = function() {
   setUserName();
+}
+
+let nome=document.querySelector('#nome');
+let cognome=document.querySelector('#cognome');
+let eta=document.querySelector('#eta');
+nome.onclick = function(){
+  
 }
